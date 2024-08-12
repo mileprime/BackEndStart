5d33c379d22cf71cd1d7223c7066f9590b96f96f
Blue-G <blue-g@Blue-Gs-iMac.local>
2024-08-12 14:49:44 +0400

    used functions

--- file.js
+++ file.js
@@ -1,3 +1,14 @@
-exports.myDateTime = function () {
-    return new Date().toLocaleString();
+// exports.myDateTime = function () {
+//     return new Date().toLocaleString();
+// };
+function add(a, b) {
+    return a + b;
 };
+function zombie(a, b, c) {
+    return a * b * c;
+};
+
+module.exports = {
+    add,
+    zombie
+}
---
+++ serverfile.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const test = require('./file');
+
+const server= http.createServer((req, res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write( "add " + test.add(2, 3)+ "\n");
+    res.write("multiply " + test.zombie(4,5,6));
+    res.end();
+});
+
+server.listen(8080, '127.0.0.1', () => {
+    console.log('http://127.0.0:8080')
+});
